241349915a7f7a0a91cdff08c0ccea26bcca001c
votre-nom <dabovalkilian@gmail.com>
2025-03-31 10:43:16 +0200

    code cercle

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,11 @@
         return (((3 * Math.Sqrt(3)) / 2) * (a * a)); //formule de l'hexa
     }
 
+double cercle(double a)
+{
+    return Math.PI * (a * a);
+}
+
     Console.WriteLine("entre ta longueur et largeur");
     double a = Convert.ToDouble(Console.ReadLine());
     double b = Convert.ToDouble(Console.ReadLine());
@@ -21,3 +26,8 @@
     double c = Convert.ToDouble(Console.ReadLine());
     double res2 = hexa(c);
     Console.WriteLine($"resultat {res2}");
+
+Console.WriteLine("entre le cercle");
+double d = Convert.ToDouble(Console.ReadLine());
+double res3 = cercle(d);
+Console.WriteLine($"resultat {res3}");
