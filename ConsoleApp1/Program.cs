f8a1885e04bef8040d73d7efef6cc94762191cb7
votre-nom <dabovalkilian@gmail.com>
2025-03-31 10:22:04 +0200

    calcule de l'air du rectangle

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,13 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-double air(double a, double b)
+double air(double a, double b) //je documente la 
 {
     return a * b;
 }
 
-Console.WriteLine("entre ta longueur et largeur");
-double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("entre ta longueur et largeur"); // ecrire la phrase
+double a = Convert.ToDouble(Console.ReadLine()); // lire
 double b = Convert.ToDouble(Console.ReadLine());
 double res = air(a,b);
 Console.WriteLine($"resultat {res}");
