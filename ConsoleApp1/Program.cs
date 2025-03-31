bb6a3fa72c48d8b4914391ef4332608b86f75d3c
votre-nom <dabovalkilian@gmail.com>
2025-03-31 10:01:34 +0200

    Ajoutez des fichiers projet.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+double air(double a, double b)
+{
+    return a * b;
+}
+
+Console.WriteLine("entre ta longueur et largeur");
+double a = Convert.ToDouble(Console.ReadLine());
+double b = Convert.ToDouble(Console.ReadLine());
+double res = air(a,b);
+Console.WriteLine($"resultat {res}");
